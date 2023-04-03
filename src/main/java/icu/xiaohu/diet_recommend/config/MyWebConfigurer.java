6ef444f3aac79756a42aa07958a7c33a3eb9bc7e
沛彪 林 <linpeibiao@163.com>
2023-04-03 19:42:07 +0800

    style: 配置注解

--- src/main/java/icu/xiaohu/diet_recommend/config/MyWebConfigurer.java
+++ src/main/java/icu/xiaohu/diet_recommend/config/MyWebConfigurer.java
@@ -1,5 +1,6 @@
 package icu.xiaohu.diet_recommend.config;
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @date 2023/04/03/ 19:39
  * @description
  */
+@SpringBootConfiguration
 public class MyWebConfigurer implements WebMvcConfigurer {
     /**
      * 配置跨越
@@ -17,9 +19,10 @@ public class MyWebConfigurer implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowCredentials(true)    //允许跨域的 cookie
+                //允许跨域的 cookie
+                .allowCredentials(true)
                 // 前端更改端口时，相应的后端需要允许新更改的端口访问，否则会出现跨域问题
-//                .allowedOrigins("http://119.91.234.58:8000", "http://localhost:8088", "http://localhost:8080")
+                // .allowedOrigins("http://119.91.234.58:8000", "http://localhost:8088", "http://localhost:8080")
                 .allowedOrigins("*")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("*");
