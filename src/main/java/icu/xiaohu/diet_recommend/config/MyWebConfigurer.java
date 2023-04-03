d1be77e0f067d117b86fbba6139e71cc478bb6bd
沛彪 林 <linpeibiao@163.com>
2023-04-03 19:41:01 +0800

    feat: 跨域

---
+++ src/main/java/icu/xiaohu/diet_recommend/config/MyWebConfigurer.java
@@ -0,0 +1,27 @@
+package icu.xiaohu.diet_recommend.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author xiaohu
+ * @date 2023/04/03/ 19:39
+ * @description
+ */
+public class MyWebConfigurer implements WebMvcConfigurer {
+    /**
+     * 配置跨越
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowCredentials(true)    //允许跨域的 cookie
+                // 前端更改端口时，相应的后端需要允许新更改的端口访问，否则会出现跨域问题
+//                .allowedOrigins("http://119.91.234.58:8000", "http://localhost:8088", "http://localhost:8080")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedHeaders("*");
+    }
+}
