24ab70349e4c62ae647b8f1eb8e49bdb86d14ec2
沛彪 林 <linpeibiao@163.com>
2023-04-13 11:41:34 +0800

    fix: 预请求跨域

--- src/main/java/icu/xiaohu/diet_recommend/intercepter/LoginInterceptor.java
+++ src/main/java/icu/xiaohu/diet_recommend/intercepter/LoginInterceptor.java
@@ -31,6 +31,10 @@ public class LoginInterceptor implements HandlerInterceptor {
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return true;
+        }
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
