f5de519ab3a2da5ca95a84c9b7b33fffcab5b6fd
沛彪 林 <linpeibiao@163.com>
2023-04-20 17:13:49 +0800

    fix: 拦截器

--- src/main/java/icu/xiaohu/diet_recommend/config/MyWebConfigurer.java
+++ src/main/java/icu/xiaohu/diet_recommend/config/MyWebConfigurer.java
@@ -22,7 +22,7 @@ public class MyWebConfigurer implements WebMvcConfigurer {
                 //允许跨域的 cookie
                 .allowCredentials(true)
                 // 前端更改端口时，相应的后端需要允许新更改的端口访问，否则会出现跨域问题
-                 .allowedOrigins("http://43.139.60.130:8086/", "http://localhost:8088", "http://localhost:8080")
+                 .allowedOrigins("http://43.139.60.130:8086/", "http://localhost:8088", "http://localhost:8086")
                 .allowedOrigins("*")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("*");
