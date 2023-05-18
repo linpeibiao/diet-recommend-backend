7573872c13a41ecb2af9ee9907d1052c34f8953a
沛彪 林 <linpeibiao2022@163.com>
2023-05-19 00:37:23 +0800

    feat: 添加afterComletion

--- src/main/java/icu/xiaohu/diet_recommend/intercepter/LoginInterceptor.java
+++ src/main/java/icu/xiaohu/diet_recommend/intercepter/LoginInterceptor.java
@@ -64,4 +64,10 @@ public class LoginInterceptor implements HandlerInterceptor {
         return true;
     }
 
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("响应结束删除 session 操作");
+        UserHolder.remove();
+    }
+
 }
