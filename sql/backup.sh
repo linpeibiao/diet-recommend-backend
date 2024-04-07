043723b39c968b2cfdbe3972545cf065740f26cb
linpeibiao <linpeibiao2022@163.com>
2024-04-07 15:32:27 +0800

    doc

--- sql/backup.sh
+++ sql/backup.sh
@@ -22,3 +22,22 @@ gzip $BACKUP_DIR/$DB_NAME-$DATE.sql
 
 # 删除过期备份文件
 find $BACKUP_DIR -type f -mtime +$DAYS_TO_KEEP -name '*.gz' -delete
+
+## 创建备份目录
+#mkdir -p $BACKUP_DIR
+#
+## 备份数据库并直接写入宿主机的备份目录
+#docker exec e57bcf0b4a33 mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > /1.sql
+#
+## 将备份文件复制到宿主机上
+#docker cp e57bcf0b4a33:/1.sql $BACKUP_DIR
+#
+## 重命名
+#mv $BACKUP_DIR/1.sql $BACKUP_DIR/$DB_NAME-$DATE.sql
+#
+## 压缩备份文件
+#gzip $BACKUP_DIR/$DB_NAME-$DATE.sql
+#
+## 删除过期备份文件
+#find $BACKUP_DIR -type f -mtime +$DAYS_TO_KEEP -name '*.gz' -delete
+
