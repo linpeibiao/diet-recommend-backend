f33c0ecfd79ee286de9d9bee420a7d2a6953b06a
linpeibiao <linpeibiao2022@163.com>
2024-04-12 16:02:53 +0800

    ci

--- docker-middle.sh
+++ docker-middle.sh
@@ -21,3 +21,6 @@ docker run -d -p 9000:9000 -p 9090:9090 --name my-minio \
   -v /home/minio/data1:/data \
   -v /home/minio/cos.json:/root/.minio/cors-config.json \
   minio/minio server /data --console-address ":9000" --address ":9090"
+
+# nginx
+docker run --name my-nginx -p 80:80 -v /home/code/diet-recommend/front-ms/dist:/usr/share/nginx/html:ro -d nginx
